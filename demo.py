0b84e7380fa9036b0a11f03e070d200174e85aab
prashant <prashantmiracle@gmail.com>
2022-11-10 07:34:55 +0530

    1

--- demo.py
+++ demo.py
@@ -2,20 +2,40 @@ from tkinter import *
 
 root = Tk()
 
+# 1 EXAMPLE
+
 # label1 = Label(root, text="hello world")
 # label1.pack()
 
-newframe = Frame(root)
-newframe.pack()
+# 2 EXAMPLE
 
-otherframe = Frame(root)
-otherframe.pack(side=BOTTOM)
+# newframe = Frame(root)
+# newframe.pack()
 
-button1 = Button(newframe, text="click here", fg="Red")
-button2 = Button(otherframe, text="click here", fg="Blue")
+# otherframe = Frame(root)
+# otherframe.pack(side=BOTTOM)
+
+# button1 = Button(newframe, text="click here", fg="Red")
+# button2 = Button(otherframe, text="click here", fg="Blue")
 
 
-button1.pack()
-button2.pack()
+# button1.pack()
+# button2.pack()
+
+
+# 3. EXAMPLE
+
+label3 = Label(root, text='First Name ')
+label4 = Label(root, text='Last Name ')
+
+entry1 = Entry(root)
+entry2 = Entry(root)
+
+label3.grid(row=0,column=0)
+label4.grid(row=1,column=0)
+
+entry1.grid(row=0,column=1)
+entry2.grid(row=1,column=1)
+
 
 root.mainloop()
